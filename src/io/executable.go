b72922979b36802aecdad5e5f1c911d506247d73
Jackson Woodruff <woodruff.jackson@gmail.com>
2020-12-04 17:29:22 +0000

    Add a native executable option

--- src/io/executable.go
+++ src/io/executable.go
@@ -1,13 +1,47 @@
 package io
 
-type Executable struct {
+import (
+	"os/exec"
+)
+
+type Executable interface {
+	Execute(Values) Values
+}
+
+// Implementation of a program that may correspond e.g.
+// to a different language.
+type Program struct {
 	name string
 }
 
-func NewExecutable(name string) Executable {
-	return Executable { name }
+func NewProgram(name string) Program {
+	return Program { name }
 }
 
-func (e Executable) ToString () string {
+func (e Program) ToString () string {
 	return e.name
+}
+
+func (p Program) Execute(inputs Values) Values {
+	// We expect the executable to have a self-timeout function.
+	out,err := exec.Command(p.name, inputs.toString()).Output()
+
+	if err != nil {
+		// Invalid values
+		return Values {0, false};
+	}
+
+	return valuesFromString(string(out))
+}
+
+// Implementation of a native (i.e. go function) program.
+type NativeExecutable struct {
+	f func(int) int
+}
+
+
+func (n NativeExecutable) Execute(inputs Values) Values {
+	in := inputs.Value
+
+	return Values{n.f(in), true}
 }
--- src/io/io.go
+++ src/io/io.go
@@ -1,26 +1,13 @@
 package io
 
 import (
-	"os/exec"
 )
-
-func get_outputs(inputs Values, executable Executable) Values {
-	// We expect the executable to have a self-timeout function.
-	out,err := exec.Command(executable.name, inputs.toString()).Output()
-
-	if err != nil {
-		// Invalid values
-		return Values {0, false};
-	}
-
-	return valuesFromString(string(out))
-}
 
 func GetOutputs(inputs []Values, executable Executable) []Values {
 	var results = make([]Values, len(inputs))
 
 	for i := 0; i < len(inputs); i ++ {
-		output := get_outputs(inputs[i], executable)
+		output := executable.Execute(inputs[i])
 		results[i] = output
 	}
 
