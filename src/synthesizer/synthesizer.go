e76523d8db163a9546f4ced54af3f89e1683b871
Jackson Woodruff <woodruff.jackson@gmail.com>
2020-12-05 10:55:50 +0000

    Fix memory issues with the synthesizer

--- src/synthesizer/synthesizer.go
+++ src/synthesizer/synthesizer.go
@@ -2,9 +2,11 @@ package synthesizer
 
 import (
 	"github.com/j-c-w/SemanticHLLVectors/src/io"
+	"fmt"
 )
 
-const MAXDEPTH = 5
+const MAXDEPTH = 32
+const DEBUG = false
 var Consts = []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
 
 // This is a simple program synthesizer that takes 
@@ -20,10 +22,15 @@ func IntSynthesize(inputs []io.Values, outputs []io.Values) Node {
 
 	for depth < MAXDEPTH {
 		// Generate functions with 'depth' operators.
-		depthFunctions := GenerateFunctionsWithDepth(depth, Consts)
+		depthFunctions := make(chan Node)
+		go GenerateFunctionsWithDepth(depth, Consts, depthFunctions)
 
 		// Check if any of these functions satisfy the IO requirements
-		for _, function := range depthFunctions {
+		for function := range depthFunctions {
+			if DEBUG {
+				fmt.Println("Checking")
+				fmt.Println(function.ToString())
+			}
 			if IOCheck(function, inputs, outputs) {
 				return function
 			}
@@ -36,33 +43,37 @@ func IntSynthesize(inputs []io.Values, outputs []io.Values) Node {
 	return nil
 }
 
-func GenerateFunctionsWithDepth(depth int, consts []int) []Node {
+func GenerateFunctionsWithDepth(depth int, consts []int, c chan Node) {
 	if depth == 0 {
-		var results []Node = make([]Node, len(consts) + 1)
-		for i, c := range consts {
-			results[i] = IntChild{c}
+		for _, con := range consts {
+			c <- IntChild{con}
 		}
 		// TODO -- more than one variable for more than
 		// one variable functions :)
-		results[len(results) - 1] = VariableChild{"x"}
-		return results
+		c <- VariableChild{"x"}
+		close(c)
+		return
 	}
 
 	// For each depth, we add every operation:
-	subTrees := GenerateFunctionsWithDepth(depth / 2, consts)
+	subTrees1 := make(chan Node)
+	go GenerateFunctionsWithDepth(depth / 2, consts, subTrees1)
 
 	// Now, add the parent trees with every combination
 	// of subtrees.
-	results := make([]Node, len(subTrees) * len(subTrees) * len(operators))
-	for i, subTree1 := range subTrees {
-		for j, subTree2 := range subTrees {
-			for k, op := range operators {
-				results[i + len(subTrees) * (j + len(subTrees) * k)] = BinaryNode{op, subTree1, subTree2}
+	for subTree1 := range subTrees1 {
+		c <- subTree1
+
+		subTrees2 := make(chan Node)
+		go GenerateFunctionsWithDepth(depth / 2, consts, subTrees2)
+		for subTree2 := range subTrees2 {
+			for _, op := range operators {
+				c <- BinaryNode{op, subTree1, subTree2}
 			}
 		}
 	}
 
-	return results
+	close(c)
 }
 
 func IOCheck(f Node, inp []io.Values, out []io.Values) bool {
--- src/synthesizer/synthesizer_test.go
+++ src/synthesizer/synthesizer_test.go
@@ -7,8 +7,16 @@ import (
 )
 
 func TestGen(t *testing.T) {
+	inputs := [3]io.Values{io.Values{1, true}, io.Values{2, true}, io.Values{3, true}};
+	outputs := [3] io.Values{io.Values{1, true}, io.Values{4, true}, io.Values{7, true}};
+
+	fun := Synthesize(inputs[:], outputs[:])
+	fmt.Println(fun.ToString())
+}
+
+func TestNext(t *testing.T) {
 	inputs := [2]io.Values{io.Values{1, true}, io.Values{2, true}};
-	outputs := [2] io.Values{io.Values{3, true}, io.Values{5, true}};
+	outputs := [2] io.Values{io.Values{2, true}, io.Values{5, true}};
 
 	fun := Synthesize(inputs[:], outputs[:])
 	fmt.Println(fun.ToString())
