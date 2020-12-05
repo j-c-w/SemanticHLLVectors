7bc63e7533acd9f03d7eb279379a7b1ec5732cfb
Jackson Woodruff <woodruff.jackson@gmail.com>
2020-12-05 00:50:17 +0000

    Change to a different synthesis approach

---
+++ src/synthesizer/mathfunction.go
@@ -0,0 +1,54 @@
+package synthesizer
+
+import "strconv"
+
+var operators = []string {"+", "/", "-", "*"};
+
+type Node interface {
+	ToString() string
+	Evaluate(int) int
+}
+
+type BinaryNode struct {
+	op string
+	child1 Node
+	child2 Node
+}
+
+type IntChild struct {
+	value int
+}
+
+type VariableChild struct {
+	name string
+}
+
+func (c IntChild) Evaluate(x int) int {
+	return c.value
+}
+
+func (b BinaryNode) Evaluate(x int) int {
+	switch b.op {
+	case "+": return b.child1.Evaluate(x) + b.child2.Evaluate(x)
+	case "*": return b.child1.Evaluate(x) * b.child2.Evaluate(x)
+	case "/": return b.child1.Evaluate(x) / b.child2.Evaluate(x)
+	case "-": return b.child1.Evaluate(x) - b.child2.Evaluate(x)
+	default: panic("Unknwon operator")
+	}
+}
+
+func (v VariableChild) Evaluate(x int) int {
+	return x
+}
+
+func (b BinaryNode) ToString() string {
+	return "(" + b.child1.ToString() + b.op + b.child2.ToString() + ")"
+}
+
+func (n IntChild) ToString() string {
+	return strconv.Itoa(n.value)
+}
+
+func (v VariableChild) ToString() string {
+	return v.name
+}
--- src/synthesizer/synthesizer.go
+++ src/synthesizer/synthesizer.go
@@ -2,33 +2,31 @@ package synthesizer
 
 import (
 	"github.com/j-c-w/SemanticHLLVectors/src/io"
+	"fmt"
 )
 
 const MAXDEPTH = 5
+var Consts = []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
 
 // This is a simple program synthesizer that takes 
 // a set of functions and tries to compose them.
 
-func Synthesize(inputs []io.Values, outputs []io.Values) *IntFunction {
+func Synthesize(inputs []io.Values, outputs []io.Values) Node {
 	return IntSynthesize(inputs, outputs)
 }
 
-func IntSynthesize(inputs []io.Values, outputs []io.Values) *IntFunction {
+func IntSynthesize(inputs []io.Values, outputs []io.Values) Node {
 	// Try combinations of the functions
 	depth := 1
 
 	for depth < MAXDEPTH {
-		// Start with just the ID function.
-		var depthFunctions = IdentityFunction[:];
-
-		for current_depth := 0; current_depth < depth; current_depth ++ {
-			depthFunctions = crossProduct(depthFunctions, IntFunctions);
-		}
+		// Generate functions with 'depth' operators.
+		depthFunctions := GenerateFunctionsWithDepth(depth, Consts)
 
 		// Check if any of these functions satisfy the IO requirements
 		for _, function := range depthFunctions {
 			if IOCheck(function, inputs, outputs) {
-				return &function
+				return function
 			}
 		}
 
@@ -39,24 +37,57 @@ func IntSynthesize(inputs []io.Values, outputs []io.Values) *IntFunction {
 	return nil
 }
 
-func IOCheck(f IntFunction, inp []io.Values, out []io.Values) bool {
+func GenerateFunctionsWithDepth(depth int, consts []int) []Node {
+	if depth == 0 {
+		var results []Node = make([]Node, len(consts) + 1)
+		for i, c := range consts {
+			results[i] = IntChild{c}
+		}
+		// TODO -- more than one variable for more than
+		// one variable functions :)
+		results[len(results) - 1] = VariableChild{"x"}
+		return results
+	}
+
+	// For each depth, we add every operation:
+	subTrees := GenerateFunctionsWithDepth(depth / 2, consts)
+
+	// Now, add the parent trees with every combination
+	// of subtrees.
+	results := make([]Node, len(subTrees) * len(subTrees) * len(operators))
+	for i, subTree1 := range subTrees {
+		for j, subTree2 := range subTrees {
+			for k, op := range operators {
+				results[i + len(subTrees) * (j + len(subTrees) * k)] = BinaryNode{op, subTree1, subTree2}
+			}
+		}
+	}
+
+	for i, r := range results {
+		if r == nil {
+			fmt.Println(len(subTrees))
+			fmt.Println(len(operators))
+			fmt.Println(i)
+			fmt.Println("FAILED")
+			panic("FAILED")
+		}
+	}
+
+	return results
+}
+
+func IOCheck(f Node, inp []io.Values, out []io.Values) bool {
+	if len(inp) != len(out) {
+		panic( "Expect input/output lengths to be equal")
+	}
 	for i := 0; i < len(inp); i ++ {
-		if f.IntSim(inp[i].Value) != out[i].Value {
+		fmt.Println(inp[i])
+		fmt.Println(out[i])
+		fmt.Println(i)
+		if f.Evaluate(inp[i].Value) != out[i].Value {
 			return false
 		}
 	}
 
 	return true
-}
-
-func crossProduct(a []IntFunction, b []Function) []IntFunction {
-	result := make([]IntFunction, len(a) * len(b))
-
-	for i := 0; i < len(a); i ++ {
-		for j := 0; j < len(b); j ++ {
-			result[i * j + j] = a[i].Compose(IntFunction{[]Function{b[j]}})
-		}
-	}
-
-	return result
 }
